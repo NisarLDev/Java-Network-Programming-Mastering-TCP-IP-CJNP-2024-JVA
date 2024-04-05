2a5a3da261218da58fc522fb6d2fc8edcd39cf83
Nisar Laftissi <107173728+NisarLDev@users.noreply.github.com>
2024-04-05 19:47:27 +0200

    Update file

--- Introduction_To_Java_NIO_Classes/TCP_UDP.java
+++ Introduction_To_Java_NIO_Classes/TCP_UDP.java
@@ -1,3 +1,15 @@
 import java.nio.channels.FileChannnel;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        InetAddress ocsaly = InetAddress.getByName(host:"www.ocsaly.com");
+        FileChannel;
+        //UDP
+        DatagramChannel;
+        //TCP
+        SocketChannel
+        ServerSocketChannel;
+    };
+};
