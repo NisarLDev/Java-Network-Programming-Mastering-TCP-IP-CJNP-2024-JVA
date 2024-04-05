7a8973a0b5b58fc840fbf58aeae6591898aacbc5
Nisar Laftissi <107173728+NisarLDev@users.noreply.github.com>
2024-04-05 15:23:35 +0200

    Update 5_Get__Host_Name_And_Address_IP._Programed_by_My_Way.java

--- 5_Get__Host_Name_And_Address_IP._Programed_by_My_Way.java
+++ 5_Get__Host_Name_And_Address_IP._Programed_by_My_Way.java
@@ -2,7 +2,7 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 public class Main{
-    public static void main(String[] args) throws UnknownHostException{
+    public static void main(String[] args) throws IOException{
         InetAddress ocsaly = InetAddress.getByName("www.ocsaly.com");
         System.out.println("HOST ADDRESS        : " + ocsaly.getHost.Address());
         System.out.println("CANONICAL HOST NAME : " + ocsaly.getHost.Address());
