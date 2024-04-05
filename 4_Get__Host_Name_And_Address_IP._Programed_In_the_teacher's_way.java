8c6746d69235ec037d989afff3040d5aa7aad5bd
Nisar Laftissi <107173728+NisarLDev@users.noreply.github.com>
2024-04-05 15:34:51 +0200

    Update 4_Get__Host_Name_And_Address_IP._Programed_In_the_teacher's_way.java
    Corrected code.

--- 4_Get__Host_Name_And_Address_IP._Programed_In_the_teacher's_way.java
+++ 4_Get__Host_Name_And_Address_IP._Programed_In_the_teacher's_way.java
@@ -1,11 +1,13 @@
 import java.net.InetAddress;
-import java.net.UnknownHostException;
+import java.io.IOException;
+//import java.net.UnknownHostException;
 
 public class Main{
-    public static void main(String[] args) throws UnknownHostException{
-        InetAddress ocsaly = InetAddress.getByName(host: "www.ocsaly.com");
-        System.out.println("HOST ADDRESS        : " + ocsaly.getHost.Address());
-        System.out.println("CANONICAL HOST NAME : " + ocsaly.getHost.Address());
-        System.out.println("HOST NAME           : " + ocsaly.getHost.Address());
+    public static void main(String[] args) throws IOException{
+        InetAddress ocsaly = InetAddress.getByName("www.startpage.com");
+        System.out.println("HOST ADDRESS        : " + ocsaly.getHostAddress());
+        System.out.println("CANONICAL HOST NAME : " + ocsaly.getCanonicalHostName());
+        System.out.println("HOST NAME           : " + ocsaly.getHostName());
+        ocsaly.isReachable(timeout: 10000);
     };
 };
