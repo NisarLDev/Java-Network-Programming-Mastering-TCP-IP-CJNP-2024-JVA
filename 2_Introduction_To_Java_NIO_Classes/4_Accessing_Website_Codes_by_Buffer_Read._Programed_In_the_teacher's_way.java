650962fee064b619fd4fa73a5f50787ff66ab65c
Nisar Laftissi <107173728+NisarLDev@users.noreply.github.com>
2024-04-07 15:07:23 +0200

    Created file

---
+++ 2_Introduction_To_Java_NIO_Classes/4_Accessing_Website_Codes_by_Buffer_Read._Programed_In_the_teacher's_way.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+import java.net.URL;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        try{
+            URL url_= new URL(spec: "https://ocsaly.com");
+            URLConnection myUrlConnetion = url.openConnection();
+            BufferReader br = new BufferReader(new InputStreamReader(myUrlConnection.getInputStream()));
+            String myLine;
+            wjile ((myLine = br.readline()) != null){
+                System.out.println(myLine);
+            };
+            br.close();
+        }  catch (IOException exception) {
+            System.out.println(exception.getMessage());
+        };
+    };
+};
